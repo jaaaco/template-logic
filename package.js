11b3efd29de54178f648a9b71b98c9653c165aa0
Jakub Wietrzyk <jakub.wietrzyk@wfirma.pl>
2016-01-16 12:46:04 +0100

    version update

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
     name: 'jaaaco:template-logic',
-    version: '0.0.2',
+    version: '0.0.3',
     // Brief, one-line summary of the package.
     summary: 'Basic logical operation helpers for Meter Blaze templates',
     // URL to the Git repository containing the source code for this package.
-    git: '',
+    git: 'https://github.com/jaaaco/template-logic.git',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
